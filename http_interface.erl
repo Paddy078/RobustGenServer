a85062fed15a557ed0c8750d56c60c9105b98930
Xaver Lohmüller <xlo@adorsys.de>
2015-12-23 10:42:06 +0100

    Implemented getter for Messages via HTTP. Doesn't read request yet, always answers with messages

--- http_interface.erl
+++ http_interface.erl
@@ -1,24 +1,40 @@
 -module(http_interface).
--export([start/1]).
+-include ("constants.hrl").
+-export([start/1, stop/0]).
 
 start(Port) ->
-  spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [{active, false}]), loop(Sock) end).
+  stop(),
+  Pid = spawn(fun () ->
+    {ok, Sock} = gen_tcp:listen(Port, [{active, false}]),
+    loop(Sock)
+  end),
+  setPid(Pid).
+
+stop() ->
+  exit(getPid(), kill).
 
 loop(Sock) ->
   {ok, Conn} = gen_tcp:accept(Sock),
-  io:format("Sock: ~p~n", [Sock]),
   Handler = spawn(fun () -> handle(Conn) end),
   gen_tcp:controlling_process(Conn, Handler),
   loop(Sock).
 
 handle(Conn) ->
-  io:format("Conn: ~p~n", [Conn]),
-  gen_tcp:send(Conn, response("Hello World")),
+  Messages = chat_server:get_new_messages(),
+  io:format("Messages: ~p~n", [Messages]),
+  gen_tcp:send(Conn, response(Messages)),
   gen_tcp:close(Conn).
 
 response(Str) ->
-    B = iolist_to_binary(Str),
-    iolist_to_binary(
-      io_lib:fwrite(
-        "HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
-        [size(B), B])).
+  B = iolist_to_binary(Str),
+  io_lib:fwrite("HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
+    [size(B), B]).
+
+
+%%% Save Pid of spawned process for later to stop correctly
+%%% TODO: Ugly as fuck. Change later.
+setPid(Pid) ->
+  put(pid, Pid).
+
+getPid() ->
+  get(pid).
